5c71b3b94b0dad1e0996e397160af8df1010deff
mattperkins <github@mattperkins.com>
2018-08-31 01:41:26 +0100

    cl

--- api.js
+++ api.js
@@ -10,64 +10,61 @@ const PORT = 3000
 const server = app.listen(PORT, listening)
 console.log(`App is running on PORT:${PORT}`)
 
-function listening() {
- console.log('Callback confirmed')
+function listening () {
+  console.log('Callback confirmed')
 }
-
 // ADD USER
-// age is optional 
+// age is optional
 app.get('/add/:user/:age?', adduser)
-function adduser(req,res){
- let data2 = req.params
- let user = data2.user
- let age = Number(data2.age)
- let reply
- if(!age){
-  reply = {
-    msg: "age is a required path/parameter" 
-   }
- } else {
-// users = object at top of file (file database)
- users[user] = age
- let data = JSON.stringify(users, null, 2)
- fs.writeFile('users.json', data, fin)
- function fin(err){
-  console.log('User added to file database')
- }
- reply = {
-  status: "success",
-  user,
-  age
- }
+function adduser (req, res) {
+  let data2 = req.params
+  let user = data2.user
+  let age = Number(data2.age)
+  let reply
+  if (!age) {
+    reply = {
+      msg: 'age is a required path/parameter'
+    }
+  } else {
+    // users = object at top of file (file database)
+    users[user] = age
+    let data = JSON.stringify(users, null, 2)
+    fs.writeFile('users.json', data, fin)
+    function fin (err) {
+      console.log('User added to file database')
+    }
+    reply = {
+      status: 'success',
+      user,
+      age
+    }
+  }
+  res.send(reply)
 }
- res.send(reply)
-}
-
 
 // SHOW ALL ENTRIES IN DB
 // show all file database entries
 app.get('/all', sendAll)
-function sendAll(req,res){
- res.send(users)
+function sendAll (req, res) {
+  res.send(users)
 }
-
 
 // SEARCH DB
 // search for user in file database
 app.get('/search/:fileDb', fileDB)
-function fileDB(req,res){
- let fileDb = req.params.fileDb
- let reply
- if(users[fileDb]){
-  reply = {
-  status: "Found",
-  fileDb,
-  age: users[fileDb]
- }
- } else {
-  reply = {
-   status: "not found",
-   fileDb
-  }
- } res.send(reply)
+function fileDB (req, res) {
+  let fileDb = req.params.fileDb
+  let reply
+  if (users[fileDb]) {
+    reply = {
+      status: 'Found',
+      fileDb,
+      age: users[fileDb]
+    }
+  } else {
+    reply = {
+      status: 'not found',
+      fileDb
+    }
+  } res.send(reply)
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // file database
 let words = {
- "lemon": 5,
- "lime": 3,
- "peach": 1
+  'lemon': 5,
+  'lime': 3,
+  'peach': 1
 }
 
 // instantiate the app
@@ -13,31 +13,31 @@ const PORT = 3000
 const server = app.listen(PORT, listening)
 console.log(`App is running on PORT:${PORT}`)
 
-function listening() {
- console.log('Callback confirmed')
+function listening () {
+  console.log('Callback confirmed')
 }
 // add static html to route
 app.use(express.static('static'))
 
 // create a user route
 app.get('/user', getUser)
-function getUser(req,res){
- res.send('User request received')
+function getUser (req, res) {
+  res.send('User request received')
 }
 // search route params example
 app.get('/search/:query', search)
-function search(req,res){
- let data = req.params
- res.send(`Search for ${data.query} received`)
+function search (req, res) {
+  let data = req.params
+  res.send(`Search for ${data.query} received`)
 }
 // adding a second route path param
 app.get('/loop/:query/:num', loop)
-function loop(req,res){
- let data = req.params
- let num = data.num
- let reply = ""
- for (var i=0; i<num; i++){
-  reply += `Print loop for ${data.query} \n`
- }
- res.send(reply)
+function loop (req, res) {
+  let data = req.params
+  let num = data.num
+  let reply = ''
+  for (var i = 0; i < num; i++) {
+    reply += `Print loop for ${data.query} \n`
+  }
+  res.send(reply)
 }
